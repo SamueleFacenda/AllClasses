fac831afbe13d3490647b587289ce193b9ff2c38
defra <samuele.facenda@gmail.com>
2021-10-16 10:15:57 +0200

    Initial commit

---
+++ src/arrayInt/GestoreCondomini.java
@@ -0,0 +1,24 @@
+package arrayInt;
+
+public class GestoreCondomini {
+    private Condominio[] arr;
+    public GestoreCondomini(Condominio[] arr){
+        this.arr=new Condominio[arr.length];
+        for(int i=0;i<arr.length;i++)
+            this.arr[i]=arr[i].getCopy();
+    }
+    public Condominio maxNumAppartamenti(){
+        int max=0;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i].getNCondomini()<arr[max].getNCondomini())
+                max=i;
+        }
+        return arr[max].getCopy();
+    }
+    public String[] getMaxMilliForApartment(){
+        String[] out=new String[arr.length];
+        for(int j=0;j<arr.length;j++)
+            out[j]="\nappartamento: "+arr[j].getNome()+"\nnome del proprietario con il maggior numero di millesimi: "+arr[j].getMaxMilli().getProprietario();
+        return out;
+    }
+}
---
+++ src/arrayInt/Main.java
@@ -0,0 +1,9 @@
+package arrayInt;
+
+public class Main {
+    public static void main(String[] args){
+       Parola p=new Parola(new char[]{'a','b','c','d','e'});
+       p=new Parola(p.codifica());
+        System.out.println(p);
+    }
+}
