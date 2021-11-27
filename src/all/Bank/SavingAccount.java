bc17477691d26055b0b3a19be129ef2e60b867d3
defra <samuele.facenda@gmail.com>
2021-11-27 11:24:05 +0100

    bank account class

---
+++ src/all/Bank/SavingAccount.java
@@ -0,0 +1,17 @@
+package all.Bank;
+
+import java.util.LinkedList;
+
+public class SavingAccount extends BankAccount{
+    private LinkedList<Double> transaction;
+    public SavingAccount(String nome){
+        super(nome);
+        transaction=new LinkedList<>();
+        transaction.add(0.0);
+    }
+    @Override
+    public void deposit(double amount){
+        setBalance(getBalance()+amount);
+        transaction.add(getBalance());
+    }
+}
