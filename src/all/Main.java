c2c9edf725f089c5be8688bb78a53ca9feb07f57
Samuele Facenda <samuele.facenda@buonarroti.tn.it>
2022-03-15 09:03:05 +0100

    GroupedBag

--- src/all/Main.java
+++ src/all/Main.java
@@ -5,24 +5,7 @@ import all.Poli.Padre;
 
 public class Main {
     public static void main(String[] args){
-      Padre p=new Padre();
-        Figlio f=new Figlio();
-        Padre pf=new Figlio();
-        Padre pp=pf;
-        System.out.println(p);
-        System.out.println(pp);
-        pp.incrementa();
-        System.out.println(p);
-        System.out.println(pp);
-        pp.incrementa();
-        System.out.println(p);
-        System.out.println(pp);
-        f.incrementa();
-        System.out.println(f);
-        pf.incrementa();
-        System.out.println(pf);
-        Figlio ff;
-        ff=(Figlio)pp;
-        System.out.println(ff);
+        System.out.println("ciao");
+
     }
 }
