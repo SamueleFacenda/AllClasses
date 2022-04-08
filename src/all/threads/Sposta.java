ad75197f8166a302b6008d8d937225b36b0dafe7
defra <samuele.facenda@gmail.com>
2022-04-08 11:07:30 +0200

    bambino

---
+++ src/all/threads/Sposta.java
@@ -0,0 +1,18 @@
+package all.threads;
+
+public class Sposta implements Runnable {
+    private static int var1=200,var2=300;
+    private int quanto;
+    public Sposta(int quanto){
+        this.quanto=quanto;
+    }
+    @Override
+    public void run(){
+        var1+=quanto;
+        var2-=quanto;
+    }
+    public static void stampaValori(){
+        System.out.println("var1: "+var1);
+        System.out.println("var2: "+var2);
+    }
+}
