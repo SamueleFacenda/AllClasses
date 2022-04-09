6b05b011aad55c45614ab3e45b2df384b38e8e74
defra <samuele.facenda@gmail.com>
2022-04-09 22:46:20 +1400

    circular queue

--- src/all/bag/MainIter.java
+++ src/all/bag/MainIter.java
@@ -2,18 +2,16 @@ package all.bag;
 
 public class MainIter {
     public static void main(String[] args) {
-        Bag<String> borsa = new all.bag.Bag<>();
-        System.out.println(borsa.isEmpty());
-        borsa.add("primo");
-        System.out.println(borsa.toString());
-        borsa.add("secondo");
-        System.out.println(borsa.toString());
-        borsa.add("secondo");
-        System.out.println(borsa.toString());
-        System.out.println(borsa.contains("secondo"));
-        System.out.println(borsa.getFrequency("secondo"));
-        borsa.clear();
-        System.out.println(borsa.toString());
-        System.out.println("    marcel");
+        CircularQueue<Integer> qu=new CircularQueue<>(5);
+        qu.enqueue(2);
+        qu.enqueue(4);
+        qu.enqueue(6);
+        qu.enqueue(8);
+        qu.enqueue(10);
+        qu.dequeue();
+        qu.dequeue();
+        qu.enqueue(23);
+        qu.enqueue(3445);
+        qu.enqueue(23);
     }
 }
