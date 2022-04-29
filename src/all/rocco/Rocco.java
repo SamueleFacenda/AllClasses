fa3cee362e19f781df37b86e54f20c3d5923362f
Samuele Facenda <samuele.facenda@buonarroti.tn.it>
2022-04-29 09:34:53 +0200

    compiti tpsit

---
+++ src/all/rocco/Rocco.java
@@ -0,0 +1,47 @@
+package all.rocco;
+
+public class Rocco {
+
+    public static int fattoriale(int n){
+        int out;
+        if( n <= 0  ){
+            out = 1;
+        }else{
+            out = n * fattoriale(n-1);
+
+        }
+        return out;
+    }
+
+
+    public static int fibonacci(int posizione){
+        if(posizione <= 2){
+            return 1;                        //caso base
+        }else{
+            return fibonacci(posizione-1) + fibonacci(posizione-2);
+        }
+
+
+    }
+    public static int fibonacciBest(int n){
+        boolean c=true;
+        int uno = 1,due= 1,i=3;
+        while(i<=n){
+            if(c)
+                uno += due;
+            else
+                due += uno;
+            c=!c;
+            i++;
+        }
+        return c ? due : uno;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println(fibonacciBest(35757));
+    }
+}
